5adcd79143168843190fa20438fe473a5c6b5c42
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2023-10-03 23:14:00 +0300

    StopGame command now returns boolean

--- src/main/java/inc/xiddy/hypixel/commands/StopGame.java
+++ src/main/java/inc/xiddy/hypixel/commands/StopGame.java
@@ -1,10 +1,10 @@
 package inc.xiddy.hypixel.commands;
 
+import inc.xiddy.hypixel.Main;
 import inc.xiddy.hypixel.constants.Permission;
 import inc.xiddy.hypixel.dataclasses.HypixelCommand;
 import inc.xiddy.hypixel.dataclasses.HypixelGame;
 import inc.xiddy.hypixel.dataclasses.HypixelPlayer;
-import inc.xiddy.hypixel.Main;
 import org.bukkit.ChatColor;
 
 import java.util.List;
@@ -19,22 +19,23 @@ public class StopGame extends HypixelCommand {
 	public boolean execute(HypixelPlayer player, String[] args) {
 		// Get games of this type
 		List<HypixelGame> games = Main.getMainHandler().getGameHandler().getAllGames();
+
 		// If there are no games running
 		if (games.isEmpty()) {
 			// Send error
 			player.sendMessage(ChatColor.DARK_RED + "No games running.");
-		} else {
-			// Otherwise,
-			// For each game
-			// CONVERT TO ARRAY SO THAT THE LIST OBJECT DOES NOT CHANGE DURING THE LOOP
-			for (HypixelGame game: games.toArray(new HypixelGame[0])) {
-				// If OP is in the game
-				if (game.getRunnableGame().getPlayers().contains(player)) {
-					// Stop the game
-					game.stopGame();
-				}
+			return true;
+		}
+
+		// CONVERT TO ARRAY SO THAT THE LIST OBJECT DOES NOT CHANGE DURING THE LOOP
+		for (HypixelGame game: games.toArray(new HypixelGame[0])) {
+			// If OP is in the game
+			if (game.getRunnableGame().getPlayers().contains(player)) {
+				// Stop the game
+				game.stopGame();
 			}
 		}
-		return false;
+
+		return true;
 	}
 }
