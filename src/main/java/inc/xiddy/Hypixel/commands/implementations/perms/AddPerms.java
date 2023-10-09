6515793a11322e2661515c001a548bf29caab3a0
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2023-10-09 23:20:15 +0300

    Created addperms command

--- src/main/java/inc/xiddy/Hypixel/commands/implementations/perms/AddPerms.java
+++ src/main/java/inc/xiddy/Hypixel/commands/implementations/perms/AddPerms.java
@@ -3,6 +3,11 @@ package inc.xiddy.hypixel.commands.implementations.perms;
 import inc.xiddy.hypixel.commands.CommandInfo;
 import inc.xiddy.hypixel.commands.HypixelCommand;
 import inc.xiddy.hypixel.dataclasses.HypixelPlayer;
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.model.user.User;
+import net.luckperms.api.node.Node;
+import org.bukkit.ChatColor;
 
 
 @CommandInfo(name = "addperms", minArgCount = 2, permission = "hypixel.admin")
@@ -13,13 +18,20 @@ public class AddPerms extends HypixelCommand {
 	public void execute(HypixelPlayer player, String[] args) {
 		String playerName = args[0];
 		String permission = args[1];
-//
-//
-//		api.getUserManager().getUser(args)
-//
-//		LuckPerms api = LuckPermsProvider.get();
 
-		player.sendMessage("test");
+		LuckPerms api = LuckPermsProvider.get();
 
+		User user = api.getUserManager().getUser(playerName);
+		if (user == null) {
+			player.sendMessage(ChatColor.DARK_RED + "Not a valid or online username.");
+			return;
+		}
+
+		user.data().add(Node.builder(permission).build());
+		api.getUserManager().saveUser(user);
+
+		player.sendMessage(
+			ChatColor.GREEN + "Permission '" + permission + "' added to " +
+				user.getUsername() + "(" + user.getUniqueId() + ")!");
 	}
 }
