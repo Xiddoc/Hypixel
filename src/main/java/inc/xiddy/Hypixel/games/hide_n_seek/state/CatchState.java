e9a60ce9a89f5111999e95cf0eb24de3c49deaf5
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2023-10-21 08:23:37 +0300

    Removed spectator handler, unused

--- src/main/java/inc/xiddy/Hypixel/games/hide_n_seek/state/CatchState.java
+++ src/main/java/inc/xiddy/Hypixel/games/hide_n_seek/state/CatchState.java
@@ -15,6 +15,4 @@ public enum CatchState implements GameState {
 
 		}
 	}
-
-
 }
--- src/main/java/inc/xiddy/Hypixel/plugin/on_enable/StartEventHandlers.java
+++ src/main/java/inc/xiddy/Hypixel/plugin/on_enable/StartEventHandlers.java
@@ -1,7 +1,6 @@
 package inc.xiddy.hypixel.plugin.on_enable;
 
 import inc.xiddy.hypixel.games.hub.HubEventHandler;
-import inc.xiddy.hypixel.games.hub.SpectatorEventHandler;
 import inc.xiddy.hypixel.plugin.OnPluginEventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,6 +9,5 @@ public class StartEventHandlers implements OnPluginEventHandler {
 	@Override
 	public void onHandleEvent(JavaPlugin plugin) {
 		new HubEventHandler();
-		new SpectatorEventHandler();
 	}
 }
